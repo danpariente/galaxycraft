b037bf18902fc8c6bec39047b0bbaf25d73344ab
Dan <dansification@gmail.com>
2023-06-05 11:11:49 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :journeys do
+     resources :students, only: :show
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :students do
+    resources :quests, only: [:show, :index]
+  end
+
+  resources :quests, only: [:new, :create]
+  resources :paths, only: :create
+
+  resources :assignments
 end
