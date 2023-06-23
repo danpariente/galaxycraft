515158ebc6173f36ddddf0c9ffc989f507deef9d
Dan <dansification@gmail.com>
2023-06-23 15:14:40 -0400

    Add API V1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :journeys, only: [] do
+        resources :quests, only: [:index]
+      end
+    end
+  end
+
   resources :journeys do
      resources :students, only: :show
   end
