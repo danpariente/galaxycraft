b1c4f1e080e0de96b304b28547ec2f0484f83952
Dan <dansification@gmail.com>
2023-07-12 11:30:09 -0400

    Update CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'http://localhost:3001', 'http://galaxycraft.solidkraft.co'
 
     resource '*',
              headers: :any,
