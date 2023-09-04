a3648b09a774af829dba7d9ff0034d6c61ecbd0b
Dan <dansification@gmail.com>
2023-09-04 08:32:15 -0400

    Modify Tokens Controller

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,21 +1,23 @@
 module API
   module V1
-    def create
-      account = Account.find_by_email(account_params[:email])
-      if account&.authenticate(account_params[:password])
-        render json: {
-          token: JsonWebToken.encode(account_id: account.id),
-          email: account.email
-        }
-      else
-        head :unauthorized
+    class TokensController < BaseController
+      def create
+        account = Account.find_by_email(account_params[:email])
+        if account&.authenticate(account_params[:password])
+          render json: {
+            token: JsonWebToken.encode(account_id: account.id),
+            email: account.email
+          }
+        else
+          head :unauthorized
+        end
       end
-    end
 
-    private
+      private
 
-    def account_params
-      params.require(:account).permit(:email, :password)
+      def account_params
+        params.require(:token).permit(:email, :password)
+      end
     end
   end
 end
