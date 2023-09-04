6c687e12c27485ccdc871d4abb8ce3a3f3401510
Dan <dansification@gmail.com>
2023-09-04 08:33:20 -0400

    Be able to create an assignment through endpoint

--- app/controllers/api/v1/assignments_controller.rb
+++ app/controllers/api/v1/assignments_controller.rb
@@ -6,6 +6,24 @@ module API
 
         render json: assignment
       end
+
+      def create
+        quest = Quest.find(params[:quest_id])
+        assignment = quest.assignments.build(assignment_params)
+        assignment.apprentice_id = params[:apprentice_id]
+
+        if assignment.save
+          render json: assignment, status: :created
+        else
+          render json: { errors: assignment.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def assignment_params
+        params.require(:assignment).permit(:submission)
+      end
     end
   end
 end
